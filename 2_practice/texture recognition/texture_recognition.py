acda51cdbab97d6f4bc9a52361ac6be9a59f7bce
Pedro Marinho <pmaf@ic.ufal.br>
2019-02-11 10:43:33 -0300

    Random walks step concluded
    The feature vector now have the activity for each node.

--- 2_practice/texture recognition/texture_recognition.py
+++ 2_practice/texture recognition/texture_recognition.py
@@ -4,6 +4,7 @@ from math import sqrt
 import numpy as np
 import os
 import copy
+import random
 
 class Node:
     def __init__(self, graylevel, posy, posx):
@@ -14,9 +15,12 @@ class Node:
         self.outdegree = 0
         self.activity = 0
         self.neighbors = []
-        
+
+    def __repr__(self):
+        return "|NODE[" + str(self.posy) + "]" + "[" + str(self.posx) + "] = " + str(self.activity) + "|"
+
 def get_nodes_list(img, nodes):
-    #mapping the pixels of the image into a list of nodes, where each node store the gray level of the pixel and the (X, Y) position 
+    #Mapping the pixels of the image into a list of nodes, where each node store the gray level of the pixel and the (X, Y) position 
     width, height = img.shape
     
     for y in range(width):
@@ -36,20 +40,20 @@ def create_link(node_tail, node_head, intensity):
 
     
 def create_links_area(node, nodes_matrix, radius, begin_y, end_y, begin_x, end_x):
-    #creating links between the 'node' and all nodes in the possible links area, based on euclidian distances less or igual to given radius
+    #Creating links between the 'node' and all nodes in the possible links area, based on euclidian distances less or igual to given radius
     for y in range(begin_y, end_y + 1):
         for x in range(begin_x, end_x + 1):
             if node != nodes_matrix[y][x] and euclidian_distance(node, nodes_matrix[y][x]) <= radius:
                 link_intensity = int(node.graylevel) - int(nodes_matrix[y][x].graylevel)
 
-                #links with negative intensity are discarded
+                #Links with negative intensity are discarded
                 if link_intensity > 0:
                     create_link(node, nodes_matrix[y][x], link_intensity)
 
 def create_dnetwork(nodes_matrix, matrix_width, matrix_height, radius):
+    #Calculating the area of possible connections with 'node'
     for line in nodes_matrix:
         for node in line:
-            #Calculating the area of possible connections with 'node'
             begin_y = node.posy - radius
             if begin_y < 0:
                 begin_y = 0
@@ -72,57 +76,89 @@ def filter_links_by_threshold(nodes_matrix, threshold):
     for line in nodes_matrix:
         for node in line:
             for neighbor in node.neighbors:
-                if neighbor[1] >= threshold:
+                if neighbor[1] > threshold:
                     node.outdegree -= 1
                     neighbor[0].indegree -= 1
                     node.neighbors.remove(neighbor)
 
+def get_node_by_weight(links_list):
+    total_weight = 0
+    current_weight = 0
+
+    for link in links_list:
+        current_weight = link[1]
+        random_number = random.randint(0, total_weight + current_weight)
+
+        if random_number >= total_weight:
+            selected = link[0]
+
+        total_weight += current_weight
+
+    return selected
+
+def random_walks(network, number_of_walks, max_walk_length):
+    for line in network:
+        for node in line:
+            #The walker starts at each node of the network 'number_of_walks' times
+            if node.outdegree > 0:
+                for i in range(number_of_walks):
+                    current_node = node
+                    for j in range(max_walk_length):
+                        #If there's no outgoing link the walker stops
+                        if current_node.outdegree > 0:
+                            current_node = get_node_by_weight(current_node.neighbors)
+                            current_node.activity += 1
+                        else:
+                            break
+
 def main():
+    #Defining the path of the image file
     filepath = os.path.join("C:\\Users\\pedro\\Documents\\UFAL\\Pesquisa\\Codigos\\gioconda\\2_practice\\texture recognition\\img", 'testeimg.jpg')
     
+    #Loading image data
     img = io.imread(filepath)
     img_width, img_height = img.shape
 
+    differents_radius = [2]
+    thresholds = [10]    
+
+    #Number of walks started at each node
+    number_of_walks = 25
+    #Maximum number of nodes visited at each walk
+    max_walk_length = 500
+
     nodes = []
+    feature_vector = []
 
     get_nodes_list(img, nodes)
     
-    #reshape the list of nodes into a matrix with the same shape of the image
+    #Reshape the list of nodes into a matrix with the same shape of the image
     initial_nodes_matrix = np.array(nodes).reshape(img_width, img_height)
     
-    differents_radius = [2, 5, 8]
-
-    feature_vector = []
-
     for radius_of_connection in differents_radius:
-
         #Copy of the original matrix of nodes to apply a specific radius
         nodes_matrix = copy.deepcopy(initial_nodes_matrix)
         
-        #create the base directed network with radius 'radius_of_connection'
+        #Create the base directed network with radius 'radius_of_connection'
         create_dnetwork(nodes_matrix, img_width, img_height, radius_of_connection)
 
-        #save the initial network to apply differents thresholds
+        #Save the initial network to apply differents thresholds
         initial_radius_network = copy.deepcopy(nodes_matrix)
 
-        thresholds = [10, 50, 100]
-        
+
         for threshold in thresholds:
+
             current_network = copy.deepcopy(initial_radius_network)
         
+            #Links are removed based on a threshold, if link intensity > threshold, the link will be discarded
             filter_links_by_threshold(current_network, threshold)
-
-
             
-            #TODO random walks step
+            #Estimate the activity of the nodes by random walks started at each node 
+            random_walks(current_network, number_of_walks, max_walk_length)
 
-
-
-
-            #feature vector with all networks with different radius and thresholds
+            #Feature vector with all networks with different radius and thresholds
             feature_vector.append((radius_of_connection, threshold, copy.deepcopy(current_network)))
 
-    print(feature_vector)
 main()
 
 
